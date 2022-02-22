9d320a05f3770972ca3f533e31735ee898cf4a06
ArthurLacerda03 <arthur03lacerda@gmail.com>
2022-02-21 22:34:01 -0300

    Aula 01

---
+++ olamundo.js
@@ -0,0 +1,8 @@
+console.log('Olá mundo')
+let a  = 2
+console.log('Esse é o conteudo de a = ',a)
+let b = 3;
+console.log('Esse é o resultado de ', a ,'+', b , '=', a+b) 
+let nome = 'Arthur'
+console.log('Meu nome é ', nome)
+console.log(nome * a) // resulta em Not a Number (NaN)
